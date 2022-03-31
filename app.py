b3d32323369bd0f4ded84ddc0d6d5c5a76903c8e
ramirezoliver <61497306+ramirezoliver@users.noreply.github.com>
2022-03-31 17:22:12 +0800

    Add Model relationships
    add model relationships

--- app.py
+++ app.py
@@ -16,12 +16,22 @@ class Customer(db.Model):
     postcode = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
 
+    orders = db.relationship('Order', backref='customer')
+
+order_product = db.Table('order_product',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    shipped_date = db.Column(db.DateTime
-    delivered_date = db.Column(db.DateTime
+    shipped_date = db.Column(db.DateTime)
+    delivered_date = db.Column(db.DateTime)
     coupon_code = db.Column(db.String(50))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    
+    products = db.relationship('Product', secondary=order_product)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
